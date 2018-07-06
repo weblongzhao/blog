d866e0261cc89237b0fdf5d997b7ba889f379a4a
weblongzhao <longzhao135@163.com>
2018-07-06 13:41:42 +0800

    Create built-express.js
    如何制作漂亮简单的Node CLI

---
+++ built-express.js
@@ -0,0 +1,24 @@
+
+const execa = require('execa');
+const Listr = require('listr');
+
+new Listr([
+	  {
+		      title: 'install express',
+		      task: () => execa('npm', ['install',"--save express"])
+		    },{
+		title:'install express-generator',
+		task:()=>execa("npm",['install','express-generator','-g'])	    
+	  },
+	  {
+		      title: 'built express pro',
+		      task: () => execa('express', ['-H -f'])
+		    },
+	  {
+		      title: 'install express dev',
+		      task: () =>
+		          execa('npm', ['install'])
+		          .catch(() => task.skip())
+		    }
+]).run();
+
